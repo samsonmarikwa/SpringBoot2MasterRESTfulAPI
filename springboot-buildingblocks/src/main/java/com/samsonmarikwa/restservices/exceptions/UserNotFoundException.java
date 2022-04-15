7fa91f33f86f1292994bda25851883b14447be40
unknown <samsonmarikwa@outlook.com>
2022-04-15 18:20:25 -0400

    JPA Associations

--- springboot-buildingblocks/src/main/java/com/samsonmarikwa/restservices/exceptions/UserNotFoundException.java
+++ springboot-buildingblocks/src/main/java/com/samsonmarikwa/restservices/exceptions/UserNotFoundException.java
@@ -1,11 +1,7 @@
 package com.samsonmarikwa.restservices.exceptions;
 
 public class UserNotFoundException extends Exception {
-   
-   private static final long serialVersionUID = 1L;
-   
    public UserNotFoundException(String message) {
       super(message);
    }
-   
 }
